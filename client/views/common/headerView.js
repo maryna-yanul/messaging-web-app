184d18077eade0bc1add597d26a41df1263408fc
Maryna Yanul <maryna.yanul@gmail.com>
2016-01-17 00:19:52 +0200

    login/registration

--- client/views/common/headerView.js
+++ client/views/common/headerView.js
@@ -3,5 +3,10 @@ Template.home.helpers({
 });
 
 Template.home.events({
-  
+  'click .exit': function(e){
+        e.preventDefault();
+        Meteor.logout(function () {
+            Router.go('userloginView')
+        });
+    },
 });
